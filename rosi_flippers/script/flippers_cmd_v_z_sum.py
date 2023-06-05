69846be34393c2515b1ea67ab8886dcf44ceec1a
filRocha <filipe.rocha@ufrj.br>
2023-06-05 11:38:22 -0300

    Updated the time window to discard messages in flippers_cmd_V_Z_sum

--- rosi_flippers/script/flippers_cmd_v_z_sum.py
+++ rosi_flippers/script/flippers_cmd_v_z_sum.py
@@ -25,7 +25,7 @@ class NodeClass():
         self.deque_queue_max_elements = 10
 
         # time window to discard last received command
-        self.timeWindowToDiscardCmd = rospy.Duration.from_sec(0.1)
+        self.timeWindowToDiscardCmd = rospy.Duration.from_sec(1)
 
         ##=== Useful variables
         # callback messages
@@ -80,7 +80,7 @@ class NodeClass():
                 m.header.frame_id = self.node_name
                 m.data = cmd_v_z_sum
                 self.pub_cmdVel_cmdVelVzPi.publish(m)
-                #print(m)
+                print(m)
 
 
     def cllbck_cmdVelVzPi_joy(self, msg): 
